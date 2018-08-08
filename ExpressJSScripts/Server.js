8c71f789a95aa2ac31660be50f11bbbea6200912
Ravi Teja <tejatheprogrammer4u@gmail.com>
2018-08-08 22:50:26 +0530

    EJS Commit

--- ExpressJSScripts/Server.js
+++ ExpressJSScripts/Server.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var NodeApp = express();
 
+NodeApp.set("view engine", "ejs");
+
 NodeApp.get("/", (request, response) => {
     // debugger;
-    var readStream = fs.createReadStream("../index.html", 'utf8');
+    var readStream = fs.createReadStream(__dirname + "/../index.html", 'utf8');
 
     readStream.pipe(response);
 });
 
 NodeApp.get("/profile/:id", (request, response) => {
-
-    response.send("I GOt your Request with Profile ID :" + request.params.id);
+    // debugger;
+    //response.send("I GOt your Request with Profile ID :" + request.params.id);
+    // response.sendFile(path.resolve(__dirname + "/../index.html"));
+    response.render('profile', { personDetails: request.params.id });
 
 });
 
--- Server.js
+++ Server.js
@@ -4,10 +4,12 @@ const fs = require("fs");
 var server = http.createServer(function (request, response) {
     debugger;
     response.writeHead(200, { 'content-type': "text/html" });
-    var readStream = fs.createReadStream(__dirname + "/index.html");
+    //var readStream = fs.createReadStream(__dirname + "/index.html");
 
-    readStream.pipe(response);
+    //sreadStream.pipe(response);
     //response.end("hi dude you at server");
+
+    response.sendFile(__dirname + "/../index.html", 'utf8');
 
 });
 server.listen(3000, 'localhost');
